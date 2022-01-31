20d2317c6fbe642e5cf62b2671766a31cd9df4d4
reinhake <reinhake@oregonstate.edu>
2022-01-30 19:34:13 -0800

    Test 2

--- tests.py
+++ tests.py
@@ -5,7 +5,22 @@ from contrived_func import contrived_func
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertTrue(contrived_func(120))
-   
+
+    def test2(self):
+        self.assertFalse(contrived_func(100))
+
+    def test3(self):
+        self.assertFalse(contrived_func(150))
+
+    def test4(self):
+        self.assertTrue(contrived_func(46))
+
+    def test5(self):
+        self.assertFalse(contrived_func(50))
+
+    def test4(self):
+        self.assertFalse(contrived_func(6))
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
